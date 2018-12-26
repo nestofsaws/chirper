6d0fafce2da08da37d27b18d15cafe04952cd2bc
Brian Zimorowicz <zims@nestofsaws.com>
2018-12-25 21:56:54 -0500

    initial commit

---
+++ footer.php
@@ -0,0 +1,8 @@
+  <footer class="footer">
+      <div class="container">
+        <p class="text-center foot-note">Copyright 2015 Imperial Web Development</p>
+      </div>
+    </footer>
+    
+ </body>
+</html>
